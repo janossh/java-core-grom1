16c261031bc44064cf438e1a56d205cdd65a7ec6
roman <jano.ssh@gmail.com>
2017-11-07 19:32:42 +0200

    lesson 30 hw

--- src/lesson30/task1/task2/Demo.java
+++ src/lesson30/task1/task2/Demo.java
@@ -3,7 +3,7 @@ package lesson30.task1.task2;
 public class Demo {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        System.out.println(controller.employeesByProject(ProjectDAO.getProject(0)));
+        System.out.println(controller.employeesByProject(ProjectDAO.getProject(0).getName()));
         //System.out.println(controller.employeesByTeamLead(EmployeeDAO.getEmployee(2)));
        // System.out.println(controller.employeesWithoutProject());
     }
