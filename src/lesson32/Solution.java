cd461816ca987270f3f6d5cd1d44c5008faf809b
roman <jano.ssh@gmail.com>
2017-11-13 19:42:38 +0200

    lesson 32

--- src/lesson32/Solution.java
+++ src/lesson32/Solution.java
@@ -16,35 +16,42 @@ public class Solution {
 
         int n = 3;
 
-        while (!countNumbers(bufferedReader.readLine()) && n != 1) {
-            System.out.println("Your numbers are wrong. You have " + (--n) + " attempts to try again");
-        }
+        do {
+            try {
+                countNumbers(bufferedReader.readLine());
+                break;
+            } catch (Exception e) {
+                if (n == 1)
+                    break;
+                System.out.println("" + e.getMessage() + " You have " + (--n) + " attempts to try again");
+            }
+        } while (n != 0);
 
         if (n == 1)
             System.out.println("Your numbers are wrong. Number of attempts exceeded");
 
     }
 
-    private static boolean countNumbers(String str) {
+    private static void countNumbers(String str) throws Exception {
         boolean flag = true;
 
-        String[] arrayList =  str.split(" ");
+        String[] arrayList = str.split(" ");
 
-        if (!isDigits(str) && arrayList.length != 10)
-            return false;
-
-        int count = 0;
-        for (String st : arrayList) {
-            if (Integer.valueOf(st) > 100)
-                return false;
-            count += Integer.valueOf(st);
-        }
-        System.out.println("Сумма 10ти чисел = " + count);
-        return flag;
+        if (isDigits(str) && arrayList.length == 10) {
+            int count = 0;
+            for (String st : arrayList) {
+                if (Integer.valueOf(st) > 100) {
+                    throw new Exception("Your numbers are wrong.");
+                }
+                count += Integer.valueOf(st);
+            }
+            System.out.println("Сумма 10ти чисел = " + count);
+        } else
+            throw new Exception("Your numbers are wrong.");
     }
 
     public static boolean isDigits(String input) {
-        Pattern p = Pattern.compile("\\d+\\s?");
+        Pattern p = Pattern.compile("[\\d+\\s?]+");
         Matcher m = p.matcher(input);
         return m.matches();
     }
