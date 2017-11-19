436ab6ff8767e4cd6461f4c0720def717a9dcd07
roman <jano.ssh@gmail.com>
2017-11-19 13:44:24 +0200

    lesson33hw1

---
+++ src/lesson33/hw1/Demo.java
@@ -0,0 +1,7 @@
+package lesson33.hw1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution.writeToFileFromConsole("C:\\3.txt");
+    }
+}
---
+++ src/lesson33/hw1/Solution.java
@@ -0,0 +1,77 @@
+package lesson33.hw1;
+
+import java.io.*;
+
+public class Solution {
+    public static void writeToFileFromConsole(String path) {
+        FileReader reader = null;
+        try {
+            reader = new FileReader(path);
+        } catch (IOException e) {
+            System.out.println("File with path " + path + " not found");
+            return;
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException e) {
+                System.out.println("Can't close file");
+            }
+        }
+
+
+        System.out.println("Enter file content to write in the file:");
+        String stringToFile = readFromKeyboard();
+        writeFile(path, stringToFile);
+    }
+
+    public static String readFromKeyboard() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String flEnd = "wr";
+        StringBuilder newLine;
+        String str = "";
+        do {
+            try {
+                str = bufferedReader.readLine();
+                newLine = new StringBuilder(str.subSequence(0, str.length()));
+                if (!flEnd.equals(str))
+                    stringBuilder.append(newLine + System.lineSeparator());
+            } catch (IOException e) {
+                System.out.println("Error read from keyboard");
+            }
+        } while (!flEnd.equals(str));
+
+        try {
+            inputStreamReader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Error to close stream reader");
+        }
+
+
+        return stringBuilder.toString();
+    }
+
+    public static void writeFile(String path, String content) {
+        FileWriter writer = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            writer = new FileWriter(path, true);
+            bufferedWriter = new BufferedWriter(writer);
+            //bufferedWriter.append(System.lineSeparator());
+            bufferedWriter.append(content);
+        } catch (IOException e) {
+            System.err.println("Can't write to file " + path);
+        } finally {
+            try {
+                bufferedWriter.close();
+                writer.close();
+            } catch (IOException e) {
+                System.err.println("Error to close writer strim");
+            }
+        }
+    }
+}
